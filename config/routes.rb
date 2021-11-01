9aff2b0f73a6d48d715a33dd82ba42e477f12854
Tanusree Modi <moditanusree1793@gmail.com>
2021-11-01 17:22:52 +0530

    Crud employee

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :employees
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :admin do
     namespace :api do
       resources :inventories
+      resources :employees
     end
   end
 end
